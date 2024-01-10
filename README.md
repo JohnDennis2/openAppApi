9482b7777497adf70bbf98b63ab404790b9a7b25
John D <johnd@Johns-MBP.attlocal.net>
2024-01-09 21:07:31 -0600

    HTML struggles and pseudo code for JS

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # openAppApi
 This is a repo for the weather app that connects with open weather API
+
+The user can search for a city.  The cities current information will be displayed
---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//First I need a fucntion that will take the user input for the city desired
+//that will fetch the information from the Open Weather API
+
+//Then I need a function that will take the information from the API
+//and Pars that info and display the information in the Current Weather container.
+
+//I need a function that will use local storage 
+//to show the past serach history of cities in a container underneath the search bar.
+
+//I need a function that will display the future weather conditons for a chosen city
+
+//I need a function that will display the past city and its information within the current city container
+
